5550ffb1cef8413414e5a284e99b93f8c47cadc7
RichardVargas1 <richard.vargas95@gmail.com>
2018-08-28 17:58:08 -0500

    Adjustments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
-
-    // Array containing the choices the computer has to make, based off of user response.
+    // Array containing the choices the computer has to make, based off of user responses.
     var computerChoices = ["r", "p", "s"];
 
     // Variables that record the score for the game. Before game starts, counters start at the number 0.
@@ -7,26 +6,26 @@
     var losses = 0;
     var ties = 0;
 
-    // Create variables that hold references to the places in the HTML where we want to display things.
-    var directionsText = document.getElementById("directions-text");
+    // These variables will display references from the user/computer choices. It also contains the references for
+    // Wins, losses, and ties.
     var userChoiceText = document.getElementById("userchoice-text");
     var computerChoiceText = document.getElementById("computerchoice-text");
     var winsText = document.getElementById("wins-text");
     var lossesText = document.getElementById("losses-text");
     var tiesText = document.getElementById("ties-text");
 
-    // This function is run whenever the user presses a key.
+    // Onkeyup is run whenever the user presses down onto key. It will record any result.
     document.onkeyup = function(event) {
 
-    // Determines which key was pressed.
+    // Helps the computer process whatever key the user touched. It will then respond with a guess of it's own:
     var userGuess = event.key;
 
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    // After the user makers their guess, the computer will then make it's own guess.
+    //The computer will only make choices from the array created on the code line #2.
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-    // Reworked our code from last step to use "else if" instead of lots of if statements.
-
-    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+    // This is the overall background of the game. This will help the computer process the results of each game.
+    // Also, after each win/loss/or tie, the game wil reset without having to hit the refresh button. The game can go on for as long as the user wants.
     if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
 
         if ((userGuess === "r") && (computerGuess === "s")) {
@@ -46,7 +45,7 @@
         }
 
 
-        // Display the user and computer guesses, and wins/losses/ties.
+        // Illustrates user/computer guesses. Including the wins, losses, and ties for bth parties.
         userChoiceText.textContent = "You chose: " + userGuess;
         computerChoiceText.textContent = "The computer chose: " + computerGuess;
         winsText.textContent = "wins: " + wins;
@@ -54,7 +53,8 @@
         tiesText.textContent = "ties: " + ties;
         // Alert notifying the user to press certain keys to play the game.
         // (This helps notify the user to turn off caps, or to not hold down the shift key).
+        // This alert pops up at anytime the user has caps lock, or the shift key, on.
     } else {
         alert("Please choose r, p, or s");
     }
-    };
+};
